75adec9408ee0c09f06bd54d470660c6908088d6
MiguelAML <miguel.ytmtz2008@gmail.com>
2025-05-29 21:36:10 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-  
+  const cart = document.getElementById("cart");
+
+const addButtons = document.querySelectorAll("#items-section button");
+
+const removeButton = document.getElementById("remove-btn");
+
+addButtons.forEach(button => {
+  button.addEventListener("click", () => {
+    const itemName = button.getAttribute("data-item");
+
+    const option = document.createElement("option");
+    option.textContent = itemName;
+    option.value = itemName;
+
+    cart.appendChild(option);
+  });
+});
+
+removeButton.addEventListener("click", () => {
+  const selected = Array.from(cart.selectedOptions);  
+
+  selected.forEach(option => {
+    cart.removeChild(option);
+  });
+});
+
